092c419cc0d0eca7753bca2326bff12d9b758014
Tushaar Gangarapu <tushaargvsg45@gmail.com>
2017-08-19 11:11:14 +0530

    Update Perceptron.py

--- Perceptron.py
+++ Perceptron.py
@@ -1,3 +1,6 @@
+# Perceptron model for IRIS.csv - Linear dataset: Gives accuracy of 100%
+# Change 'IRIS-Setosa' to 'Yes'; Line 25; to use it on SPECTF_New.csv - Non Linear dataset: Gives varied accuracy
+
 from csv import reader
 from random import randrange, seed
 
